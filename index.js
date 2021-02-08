c8781b63dc0d6125e538e16535eab304529a6818
tony-coder-18 <bill.llach1801@gmail.com>
2021-02-08 14:12:14 -0500

    Finish functionality with js

--- index.js
+++ index.js
@@ -36,6 +36,28 @@ function actions(key) {
 		let snare= new Audio("sounds/snare.mp3");
 		snare.play();
 		break;
+		case "s":
+		let tom1= new Audio("sounds/tom-1.mp3");
+		tom1.play();
+		break;
+		case "d":
+		let tom2= new Audio("sounds/tom-2.mp3");
+		tom2.play();
+		break;
+		case "j":
+		let tom3= new Audio("sounds/tom-3.mp3");
+		tom3.play();
+		break;
+		case "k":
+		let tom4= new Audio("sounds/tom-4.mp3");
+		tom4.play();
+		break;
+		case "l":
+		let crash= new Audio("sounds/crash.mp3");
+		crash.play();
+		break;
+		default:
+			console.log(key);
 	}
 	
 }
